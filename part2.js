6741d1b15977db11f0f93449ab519be938d94905
Timoor <qwertypoiuasdf@outlook.com>
2022-03-19 14:18:48 +0000

    Part c

--- part2.js
+++ part2.js
@@ -1,25 +1,15 @@
 // let tasks: Array<string> alternative 
-let tasks: string[] = [];
-
+var tasks = [];
 addTask("Take out bins");
 listAllTasks();
 
-
-function addTask(task:string){
-
+function addTask(task) {
     tasks.push(task);
-    console.log ("Task "+ task + "inserted into array\n");
+    console.log("Task " + task + "inserted into array\n");
     console.log("Length of the array: " + tasks.length);
-
 }
-
-function listAllTasks(){
-
-    for(let i = 0; i < tasks.length; i++){
-
+function listAllTasks() {
+    for (var i = 0; i < tasks.length; i++) {
         console.log(tasks[i]);
-
     }
-
-
 }
